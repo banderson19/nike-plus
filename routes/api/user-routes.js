67934ba7c0a7ebcabc0a0069cd12737a3ac5317b
Tess Austin <tessaustin92@gmail.com>
2021-04-29 17:45:08 -0600

    update get id route in user-routes w/include attend lines

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User } = require('../../models');
+const { User, Post, Attend } = require('../../models');
 
 // GET /api/users
 router.get('/', (req, res) => {
@@ -19,7 +19,19 @@ router.get('/:id', (req, res) => {
         attributes: { exclude: ['password'] },
         where: {
             id: req.params.id
-        }
+        },
+        include: [
+            {
+                model: Post,
+                attributes: ['id', 'title', 'post_content', 'created_at']
+            },
+            {
+                model: Post,
+                attributes: ['title'],
+                through: Attend,
+                as: 'attended_posts'
+            }
+        ]
     })
         .then(dbUserData => {
             if (!dbUserData) {
