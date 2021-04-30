0edf0bc759782a047bfdaa608a311ac307902689
Tess Austin <tessaustin92@gmail.com>
2021-04-29 18:15:05 -0600

    individual hooks set to true in user-routes  update'

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -85,6 +85,7 @@ router.post('/login', (req, res) => {
 // PUT /api/users/1
 router.put('/:id', (req, res) => {
     User.update(req.body, {
+        individualHooks: true,
         where: {
             id: req.params.id
         }
