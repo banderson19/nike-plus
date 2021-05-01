cc282c072d736e9a3f814a434515cd4b875e7e5b
Tess Austin <tessaustin92@gmail.com>
2021-04-30 19:39:21 -0600

    trying to fix error altering code

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -103,7 +103,8 @@ router.put('/upattend', (req, res) => {
 router.put('/:id', (req, res) => {
     Post.update(
         {
-            title: req.body.title
+            title: req.body.title,
+            post_content: req.body.post_content
         },
         {
             where: {
