7b3ad82aad79baf8bfad681244c0a8fffeac18a0
Tess Austin <tessaustin92@gmail.com>
2021-04-30 18:18:11 -0600

    comment model associations added

--- models/index.js
+++ models/index.js
@@ -39,5 +39,20 @@ User.hasMany(Attend, {
 Post.hasMany(Attend, {
   foreignKey: 'post_id'
 });
+Comment.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id'
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'post_id'
+});
 
 module.exports = { User, Post, Attend };
