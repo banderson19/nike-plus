e07562c768476d64fee128b659b9a6648c4cad2a
Tess Austin <tessaustin92@gmail.com>
2021-05-03 12:14:44 -0600

    resembles upattend logic in comment.js preventing users from submitting empty strings

--- public/javascript/comment.js
+++ public/javascript/comment.js
@@ -4,10 +4,27 @@ async function commentFormHandler(event) {
     const comment_info = document.querySelector('textarea[name="comment-body"]').value.trim();
 
     const post_id = window.location.toString().split('/')[
-        window.location.toString().split('/').length -1
+        window.location.toString().split('/').length - 1
     ];
 
-    console.log(comment_info, post_id);
+    if (comment_info) {
+        const response = await fetch('/api/comments', {
+            method: 'POST',
+            body: JSON.stringify({
+                post_id,
+                comment_info
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        if (response.ok) {
+            document.location.reload();
+        } else {
+            alert(response.statusText);
+        }
+    }
 }
 
 document.querySelector('.comment-form').addEventListener('submit', commentFormHandler);
