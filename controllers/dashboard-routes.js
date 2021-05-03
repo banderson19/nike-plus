94bd35a64f6286aa78890a6d845c51af5990425c
Tess Austin <tessaustin92@gmail.com>
2021-05-03 14:02:46 -0600

    dashboard-routes created under controllers

---
+++ controllers/dashboard-routes.js
@@ -0,0 +1,38 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Post, User, Comment } = require('../models');
+
+router.get('/', (req, res) => {
+    Post.findAll({
+        where: {
+            user_id: req.session.user_id
+        },
+        attributes: [
+            'id',
+            'title',
+            'post_content',
+            'created_at',
+            [sequelize.literal('(SELECT COUNT(*) FROM attend WHERE post.id = attend.post_id)'), 'attend_count']
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_info', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbPostData => {
+        const posts = dbPostData.map(post => post.get({ plain: true }));
+        res.render('dashboard', { loggedOn: true });
+    })
+});
+
+module.exports = router;
