1e49e2e62e96300b085e2ddbe2a0b8ed105d7d21
ogMav8 <brad.anderson019@gmail.com>
2021-05-05 11:16:59 -0600

    displaying coords from database on maps

--- public/javascript/map.js
+++ public/javascript/map.js
@@ -1,21 +1,15 @@
 
 async function initMap() {
-    
+
     var options = {
         disableDefaultUI: true,
         zoom: 8,
-        center: {lat: 40.7608, lng: -111.8910}
+        center: { lat: 40.7608, lng: -111.8910 }
     }
     // New map
     var map = new google.maps.Map(document.getElementById('map'), options)
 
-    // Listen for click on map
-    // google.maps.event.addListener(map, 'click',
-    //     function (event) {
-    //         // add marker
-    //         addMarker({coords: event.latLng})
-    //     }
-    // )
+    markers = []
     if (map) {
         console.log('hello')
         await fetch('/api/trailhead', {
@@ -24,56 +18,63 @@ async function initMap() {
                 'Content-Type': 'application/json'
             }
         })
-        .then( res => res.json())
-        .then(json => {
-            console.log(json)
-        })
+            .then(res => res.json())
+            .then(json => {
+                markers.push(json)
+            })
     }
-
-// var markers = [
-//     {
-//         coords: {lat: 40.70700, lng: -111.7592},
-//         iconImage: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
-//         content: '<h1> Granduer Peak</h1>'
-//     },
-//     {
-//         coords: {lat: 40.7976, lng: -111.8537},
-//         content: '<h1> Bobsled</h1>'
-//     },
-//     {
-//         coords: {lat: 40.5059, lng: -111.8454},
-//         content: '<h1> Corner Canyon </h1>'
-//     }
-// ]
-
-// Loop through markers
+    console.log(markers[0])
+    // Loop through markers
+    markers[0].map( e => {
+        const body = {
+            trailName: e.trail_name,
+            coords: {
+                lat:  + e.latitude,
+                lng: + e.longitude,
+            },
+            dog_friendly: e.dog_friendly
+        }
+        console.log('body', body)
+        addMarker(body)
+    })
+    // Add Marker Function
+    function addMarker(props) {
+        console.log('props', props)
+        var marker = new google.maps.Marker({
+            position: props.coords,
+            map: map,
+        })
+        console.log('111', props.trailName)
+        //Check content
+        if (props.trailName) {
+            var infoWindow = new google.maps.InfoWindow({
+                content: `<h1>${props.trailName}</h1>`
+            })
+            
+            marker.addListener('click', function () {
+                console.log('hit')
+                infoWindow.open(map, marker)
+            })
+        }
+    }
+}
 // for (var i = 0; i < markers.length; i++) {
 //     // add marker
 //     addMarker(markers[i])
 // }
 
-// // Add Marker Function
-//     function addMarker(props) {
-//         var marker = new google.maps.Marker({
-//             position: props.coords,
-//             map: map,
-//             // icon: props.iconImage
-//         })
-//         // Check for custom icon
-//         if(props.iconImage) {
-//             // set icon image
-//             marker.setIcon(props.iconImage)
-//         }
-
-//         //Check content
-//         if(props.content) {
-//             var infoWindow = new google.maps.InfoWindow({
-//                 content: props.content
-//             })
-
-//             marker.addListener('click', function() {
-//                 infoWindow.open(map, marker)
-//             })
-//         }
-//     }
-}
+// var markers = [
+    //     {
+        //         coords: {lat: 40.70700, lng: -111.7592},
+        //         iconImage: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
+        //         content: '<h1> Granduer Peak</h1>'
+        //     },
+        //     {
+            //         coords: {lat: 40.7976, lng: -111.8537},
+            //         content: '<h1> Bobsled</h1>'
+                            //     },
+                            //     {
+                            //         coords: {lat: 40.5059, lng: -111.8454},
+                            //         content: '<h1> Corner Canyon </h1>'
+                            //     }
+                            // ]
