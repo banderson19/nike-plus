00999ec5dd3d4fcf8ec6b6c464a19e82e3b1b979
Tess Austin <tessaustin92@gmail.com>
2021-05-05 14:47:00 -0600

    date now shows, time is on 24 hour trying to get onto 12 hour

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -4,9 +4,10 @@ async function newFormHandler(event) {
     const title = document.querySelector('input[name="post-title"]').value;
     const post_canyon = document.querySelector('input[name="post-canyon"]').value;
     const post_time = document.querySelector('input[name="post-time"]').value;
+    const post_date = document.querySelector('input[name="post-date"]').value;
     const post_activity = document.querySelector('input[name="post-activity"]').value;
 
-    const post_content = ` ${post_canyon} @ ${post_time} , ${post_activity}`;
+    const post_content = `: ${post_canyon} Canyon @${post_time} ${post_date}, ${post_activity}`;
 
     const response = await fetch(`/api/posts`, {
         method: 'POST',
