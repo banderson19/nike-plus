90b904c8ddeb0fb56e53ad1626d2e3394c4ac196
Tess Austin <tessaustin92@gmail.com>
2021-05-05 14:11:47 -0600

    time input will only allow time & UI is more friendly with seperate input for trail head canyon time and activity

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -2,7 +2,11 @@ async function newFormHandler(event) {
     event.preventDefault();
 
     const title = document.querySelector('input[name="post-title"]').value;
-    const post_content = document.querySelector('input[name="post-content"]').value;
+    const post_canyon = document.querySelector('input[name="post-canyon"]').value;
+    const post_time = document.querySelector('input[name="post-time"]').value;
+    const post_activity = document.querySelector('input[name="post-activity"]').value;
+
+    const post_content = ` ${post_canyon} @ ${post_time} , ${post_activity}`;
 
     const response = await fetch(`/api/posts`, {
         method: 'POST',
