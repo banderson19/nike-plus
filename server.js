227cfc11008d1406791e5a58456ef2c190c9664c
Tess Austin <tessaustin92@gmail.com>
2021-05-07 20:11:31 -0600

    uploading code tried to get icon to show

--- server.js
+++ server.js
@@ -27,6 +27,7 @@ const hbs = exphbs.create({ helpers });
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+app.use(express.static("public/images"));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
