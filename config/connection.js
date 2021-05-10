3e41e3a36581a3af312c6d1c819e9b4856cab8e5
afatasi26 <afatasi26@gmail.com>
2021-05-09 21:23:11 -0600

    stylish

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 
 require('dotenv').config();
-
+console.log(process.env.DB_NAME);
 // create connection to our db
 const sequelize = process.env.JAWSDB_URL
     ? new Sequelize(process.env.JAWSDB_URL)
